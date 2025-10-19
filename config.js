e024f18f70320d51f9708faa2751a3b9a4a5eda9
TrungTayNA <hokhanhhokhanh2006@gmail.com>
2025-10-19 12:01:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = ' Bà Nội';
 const giftUrl = '';
 const eventName = 'Chúc Mừng 20/10';
 const titleCard = 'Tặng Bà ';
-const contentCard = 'Nhân dịp ngày 20/10. Cháu chúc bà luôn luôn vui vẻ và nở những nụ cười trên môi, mong điều tốt đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau. Chúc bà luôn mạnh khỏe sống thọ với con cháu, luôn vui tươi và hạnh phúc ạ!!';
+const contentCard = 'Nhân dịp ngày 20/10. Cháu chúc bà luôn luôn vui vẻ và nở những nụ cười trên môi, mong điều tốt đẹp nhất sẽ đến với bà trong hôm nay và cả những ngày sau. Chúc bà luôn mạnh khỏe sống thọ với con cháu, luôn vui tươi và hạnh phúc ạ!!';
 
 
 const giftImage = 'ba.jpg';
