ae6d40c3bd875845a912fbb35b1e8bac6c0bd700
TrungTayNA <hokhanhhokhanh2006@gmail.com>
2025-10-19 11:37:05 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thành';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20/10';
+const titleCard = 'Tặng Mẹ ';
+const contentCard = 'Nhân dịp ngày 20/10. Con chúc mẹ luôn luôn vui vẻ và nở những nụ cười trên môi, mong điều tốt đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau. Chúc mẹ trẻ mãi không già, luôn vui tươi và hạnh phúc ạ!!';
+
+
+const giftImage = 'me.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
