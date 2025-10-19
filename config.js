7341ed4e52a612b80fee2b6087f7d17920afcbe2
TrungTayNA <hokhanhhokhanh2006@gmail.com>
2025-10-19 04:52:26 +0000

    thaynhacvaanh

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Tặng Mẹ ';
 const contentCard = 'Nhân dịp ngày 20/10. Con chúc mẹ luôn luôn vui vẻ và nở những nụ cười trên môi, mong điều tốt đẹp nhất sẽ đến với mẹ trong hôm nay và cả những ngày sau. Chúc mẹ trẻ mãi không già, luôn vui tươi và hạnh phúc ạ!!';
 
 
-const giftImage = 'me.jpg';
+const giftImage = 'ba.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
